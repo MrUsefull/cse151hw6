c811a0e34026243e44a2c4f5839067d650aac0a0
Hannah Burkhardt <Hannah@Macademia.local>
2013-06-05 15:11:01 -0700

    .

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -1,12 +1,25 @@
 import math
 
-def errOft(t):
-	
+# h[] is a list of classifiers. -- we should be able 
+# to use each h[t] to classify an email x: h[t](x)
+
+#errOft = error of h[t] on distribution Dt
+def errOft(ht):	
 	return
 
-def alphaOft(t): 
+#alpha is a measure of weight on classifier h[t]
+def alphaOft(t): 	
+	return .5 * log((1 - errOft(h[t])) / errOf(h[t]))
+
+def final_classifier(x):
+	sum = 0
+	for t in range(1, T):
+		sum = sum + alphaOft(t)*h[t](x)
 	
-	return .5 * log((1 - errOft(t)) / errOf(t))
+	if sum < 0:
+		return -1
+	return 1
+
 
 f = open("hw6train.txt", "r")
 training_data = []
