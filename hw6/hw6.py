7be2b1b8cbf004721cf5f521ce515ff2ccfd6e0c
Hannah Burkhardt <Hannah@Macademia.local>
2013-06-05 15:35:31 -0700

    .

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -3,18 +3,29 @@ import math
 # h[] is a list of classifiers. -- we should be able 
 # to use each h[t] to classify an email x: h[t](x)
 
-#errOft = error of h[t] on distribution Dt
-def errOft(ht):	
-	return
+#errOft = error of h[t] on distribution D
+#is the distribution training_data?
+def errOft(h, D):	
+	mistakes = 0
+	for t in D:
+		if h(t) is not D[t][-1] : #D[t][-1] is the actual label of x 
+								  #(last element in email vector)
+			mistakes = mistakes + 1
+	return mistakes/len(D)
 
 #alpha is a measure of weight on classifier h[t]
-def alphaOft(t): 	
-	return .5 * log((1 - errOft(h[t])) / errOf(h[t]))
+def alphaOft(t, D): 	
+	return .5 * log((1 - errOft(h[t], D)) / errOf(h[t], D))
+
+#returns result of classifying example x with classifier t
+def h(t, x):
+
+	return
 
 def final_classifier(x):
 	sum = 0
 	for t in range(1, T):
-		sum = sum + alphaOft(t)*h[t](x)
+		sum = sum + alphaOft(t)*h(t, x)
 	
 	if sum < 0:
 		return -1
