d104239693511240c076a800f799c1bb57e3e60c
Jon C <jon@Fermat>
2013-06-05 14:54:00 -0700

    minor fixes, add stub for dOft

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -1,7 +1,13 @@
-f = open("hw6train.txt", r)
+import math
+
+def dOft(t): 
+	
+	return;
+
+f = open("hw6train.txt", "r")
 training_data = []
 line = f.readline()
 while (line):
-	training_data.add(line.split())
+	training_data.append(line.split())
 	line = f.readline()
 
